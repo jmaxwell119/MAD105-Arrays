b820776fccd6cdfec166362d0da76ad0e6966bf7
Jason Maxwell <jmaxwell119@students.mchenry.edu>
2020-09-07 21:37:12 -0500

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var aMyArray = arrayOf("ice cream", "sprinkles", "chocolate syrup", "whipped cream", "maraschino cherries", "banana", "walnuts")
+    var counter = 0
+    while (counter < aMyArray.size){
+        println(aMyArray[counter])
+        counter ++
+    }
+}
